c1903755d27b97526fe353a1fbd5fd6c2dd08140
dhilipsekar <34595289+dhilipsekar@users.noreply.github.com>
2017-12-16 14:43:00 +0530

    Add files via upload

---
+++ large.py
@@ -0,0 +1,17 @@
+a=int(input("enter the  no one to find large on it\n"))
+
+b=int(input("enter the  no two to find large on it\n"))
+
+c=int(input("enter the  no three to find large on it\n"))
+
+if (a>b and a>c):
+
+	print "the biggest no is" + str(a)
+
+if(b>a and b>c):
+	
+	print "the biggest no is " + str(b)
+
+if (c>a and c>b):
+	
+	print "the biggest no is" + str(c)
