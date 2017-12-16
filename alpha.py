8d6aa1c5c26882effa87f064dedb5610dee6d05d
dhilipsekar <34595289+dhilipsekar@users.noreply.github.com>
2017-12-16 14:39:33 +0530

    Add files via upload

---
+++ alpha.py
@@ -0,0 +1,8 @@
+n=raw_input("enter the value\n")
+if n in ("a","b","c","d","e","f","g","h","i","j","k","l","m","o","p","q","r","s","t","u","v","w","x","y","z"):
+
+	print "alphabet"
+
+else:
+	
+	print "number or special key value is enter"
