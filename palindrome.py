9b690725dabb0d7055018dd497da72c6ab5297b6
dhilipsekar <34595289+dhilipsekar@users.noreply.github.com>
2018-06-13 22:49:42 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -2,7 +2,7 @@ n=input()
 if n.isdigit():
 	n=int(n)
 	temp=n
-	if n<=1000:
+	if n<=10000:
 		sum=0
 		while(n>0):
 			rem=n%10
@@ -14,6 +14,6 @@ if n.isdigit():
 			print("no")
 			
 	else:
-		print ("enter the no between 1 to 1000")
+		print ("enter the no between 1 to 10000")
 else:
 	print ("Invalid Input")
