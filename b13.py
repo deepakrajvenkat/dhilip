5e89a5ea51a222693918bac133d7bf947256d253
dhilipsekar <34595289+dhilipsekar@users.noreply.github.com>
2018-06-19 21:32:30 +0530

    Update b13.py

--- b13.py
+++ b13.py
@@ -1,13 +1,8 @@
-n=input()
-if n.isdigit ():
-	n=int()
-	for i in range (2,n):
-		
-		if (n%i==0):
-			print ("yes")
-			break
-		else:
-			print ("no")
-			break
-else:
-	print ("invalid input")
+a=int(input())
+for i in range(2,a):
+	if (a%i==0):
+		print("no")
+		break
+	else:
+		print("yes")
+		break
