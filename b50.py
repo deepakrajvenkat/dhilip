496f23e306c41999e29da46243a348ca06eeda19
dhilipsekar <34595289+dhilipsekar@users.noreply.github.com>
2018-06-22 09:43:20 +0530

    Update b50.py

--- b50.py
+++ b50.py
@@ -1,10 +1,15 @@
 a=int(input())
 f=0
-while(a!=0):
-	a=a/2
-	if a==1:
-		f=1
-if f!=1:
-	print ("no")
-else:
+if a==1:
 	print ("yes")
+else:
+	
+	while(a!=0):
+		a=a/2
+		if a==1:
+			f=1
+	if f!=1:
+		print ("no")
+	else:
+		print ("yes")
+
