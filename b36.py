062f5449fe468afa4a1885ff90a92286efd2cf72
dhilipsekar <34595289+dhilipsekar@users.noreply.github.com>
2018-09-04 21:03:23 +0530

    Update b36.py

--- b36.py
+++ b36.py
@@ -1,6 +1,6 @@
 s=input()
 count=0
 for i in s:
-	if(i=='!' or i=='@' or i=='#' or i=='$' or i=='%' or i=='^' or i=='&' or i=='*' or i=='_'):
+	if(i=='!' or i=='@' or i=='#' or i=='$' or i=='%' or i=='^' or i=='&' or i=='*' or i=='.'):
 		count+=1
 print(count)
