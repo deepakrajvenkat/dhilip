38fd1e8d8cf21ace542b66c42f8678d82a82fbab
dhilipsekar <34595289+dhilipsekar@users.noreply.github.com>
2018-09-15 22:11:12 +0530

    Create p15.py

---
+++ p15.py
@@ -0,0 +1,16 @@
+n=input()
+check=[]
+c=[]
+maxi=1
+for i in range(len(n)):
+	count=0
+	if n[i] in check:
+		continue
+	check.append(n[i])
+	for j in range(len(n)):
+		if(n[i]==n[j]):
+			count=count+1
+		if(count>maxi):
+			maxi=count
+			character=n[i]
+print(character)			
