1c0572362a88879a2b04d9f4d6ea242bf36f8cdd
dhilipsekar <34595289+dhilipsekar@users.noreply.github.com>
2018-09-17 08:57:28 +0530

    Update p20.py

--- p20.py
+++ p20.py
@@ -1,9 +1,20 @@
-n=input()
 l=list(n)
 l2=[]
 for i in range(len(l)):
 	s=ord(l[i])
 	s=s+3
-	c=chr(s)
-	l2.append(c)
+	if(s>65 and s<90):
+		p=chr(s)
+		l2.append(p)
+	if(s>90 and s<97):
+		s=s-26
+		p=chr(s)
+		l2.append(p)
+	if(s>97 and s<122):
+		p=chr(s)
+		l2.append(p)
+	if(s>122):
+		s=s-26
+		p=chr(s)
+		l2.append(p)
 print("".join(l2))
