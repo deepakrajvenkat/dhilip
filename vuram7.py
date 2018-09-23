ddaab05393d61a1076326691945c7ed93fae2430
dhilipsekar <34595289+dhilipsekar@users.noreply.github.com>
2018-09-23 17:59:37 +0530

    Create vuram7.py

---
+++ vuram7.py
@@ -0,0 +1,8 @@
+divident=int(input("divide the two no without using *  "))
+divisor=int(input())
+count=0
+while(divident>=divisor):
+	divident=divident-divisor
+	count=count+1
+print(count)
+print(divident)
