3de616f13dde0dadbb392d9f7370cc7f6473b25d
dhilipsekar <34595289+dhilipsekar@users.noreply.github.com>
2018-10-01 11:43:57 +0530

    Create p31.py

---
+++ p31.py
@@ -0,0 +1,12 @@
+n=input().split()
+count1=0
+count2=0
+for i in range(len(n)):
+	if(n[i]=='('):
+		count1+=1
+	elif(n[i]==')'):
+		count2+=1
+if(count1==count2):
+	print("yes")
+else:
+	print("no")
